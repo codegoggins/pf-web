da4cae959a10ee9d0390c0e11fae8c2edeb48ffc
Nilay Singh <snilay011@gmail.com>
2023-01-21 16:57:40 +0530

    Main Section Completed

---
+++ src/components/Main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import emoji from "react-easy-emoji";
+const Main = () => {
+  return (
+    <div name='main' 
+        className='h-screen w-full bg-gradient-to-b from-black to-gray-800 p-6 flex flex-col items-center gap-6 md:flex-row md:items-center'>
+           {/* LEFT */}
+                <div className='text-white mt-8 flex flex-col gap-6 mb-6 md:flex-1'>
+                    <div className='flex gap-2 items-center'>
+                        <h1 className='text-3xl md:text-4xl'>
+                        I'm Nilay Singh
+                        </h1>
+                        <span className='text-3xl animate-bounce z-20'>{emoji("👋")}</span>
+                    </div>
+                    <div className='leading-7 flex flex-col items-center justify-start gap-8 mt-2 md:flex-row md:text-2xl'>
+                        <p>A passionate Full Stack Software Developer 🚀 having an experience of building Web Applications with JavaScript / Reactjs / Nodejs / MongoDB and some other cool libraries and frameworks.</p>
+                     </div>
+                     <div className='flex gap-3'>
+                        <button className='cursor-pointer font-semibold bg-black p-2.5 px-4 rounded-md hover:bg-white hover:text-black transition ease-linear duration-300'>Portfolio</button>
+                        <button className='cursor-pointer font-semibold bg-black p-2.5 px-4 rounded-md hover:bg-white hover:text-black transition ease-linear duration-300'>Resume</button>
+                     </div>
+                </div>
+            {/* LEFT */}
+
+            {/* RIGHT */}
+                <div className='flex items-center justify-center md:flex-1'>
+                    <img 
+                    src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                    alt=""
+                    className='h-56 w-96 rounded-3xl md:h-72 md:w-128'
+                     />
+                </div>
+            {/* RIGHT */}
+    </div>
+  )
+}
+
+export default Main
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,24 +33,24 @@ const Navbar = () => {
   return (
     <div className='flex items-center w-full h-20 bg-black text-white justify-between py-4 px-10'>
         <h1 className='text-4xl ml-3 font-bold'>Portfolio</h1>
-        <ul className='hidden md:flex gap-10'>
+        <ul className='hidden lg:flex gap-10'>
              {
                 navLinks.map((navLink)=>(
-                    <li key={navLink.id} className='cursor-pointer font-medium text-gray-500 text-xl capitalize hover:scale-105 duration-200'>{navLink.link}</li>
+                    <li key={navLink.id} className='cursor-pointer font-medium text-white text-xl capitalize hover:scale-105 duration-200'>{navLink.link}</li>
                 ))
              }
         </ul>
-        <div className='cursor-pointer text-gray-500 z-10 md:hidden' onClick={()=>setToggle(!toggle)}>
+        <div className='cursor-pointer text-white z-40 lg:hidden' onClick={()=>setToggle(!toggle)}>
         {
             toggle ? (<CloseOutlinedIcon fontSize='large'/>) : (<MenuOutlinedIcon fontSize='large'/>)
         }
         </div>
         {/* MOBILE MENU */}
         { toggle && (
-        <ul className='flex flex-col items-center justify-center gap-5 absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800'>
+        <ul className='flex flex-col items-center justify-center gap-5 absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 z-30'>
              {
                 navLinks.map((navLink)=>(
-                    <li key={navLink.id} className='cursor-pointer font-medium text-gray-500 text-xl capitalize hover:scale-105 duration-200'>{navLink.link}</li>
+                    <li key={navLink.id} className='cursor-pointer font-medium text-white text-2xl capitalize hover:scale-105 duration-200'>{navLink.link}</li>
                 ))
              }
         </ul>
